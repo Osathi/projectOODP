ac4b784e82ef3c4dea0574ed145f274cc6ea9669
Osathi <122779593+Osathi@users.noreply.github.com>
2023-04-28 08:10:44 +0000

    initial commit by T

---
+++ src/main/java/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Hello OODPPPPP");
+        System.out.println("Hello OODPPPPP");
+        System.out.println("Hello OODPPPPP");
+
+        TestClass T = new TestClass();
+        T.printnumber();
+    }
+}
---
+++ src/main/java/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+
+    private int number;
+
+    TestClass()
+    {
+        number = 6;
+    }
+    
+    void printnumber()
+    {
+        System.out.println(number);
+    }
+}
